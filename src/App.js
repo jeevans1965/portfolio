9df5a690a408a2f750a2e1817f28e3eb0efbf3f6
Jeevan <jeevanshivaraj2001@gmail.com>
2023-10-09 00:15:21 +0530

    modified:   src/App.js 	modified:   src/assets/image.png 	modified:   src/components/Intro/intro.css 	new file:   src/components/skills/skill.css 	new file:   src/components/skills/skill.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
+import Skill from "./components/skills/skill";
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Intro/>
+     <Skill/>
     </div>
   );
 }
---
+++ src/components/skills/skill.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './skill.css';
+
+const Skill = () => {
+  return (
+    <section id='skills'>
+      About Section
+    </section>
+  )
+}
+
+export default Skill
