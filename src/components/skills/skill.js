08698a75ef6a95a392a992af617993f4bf944aa7
Jeevan <jeevanshivaraj2001@gmail.com>
2023-10-09 15:22:59 +0530

    modified:   src/components/NavBar/navbar.css 	modified:   src/components/skills/skill.css 	modified:   src/components/skills/skill.js

--- src/components/skills/skill.js
+++ src/components/skills/skill.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import './skill.css';
+import UIDesign from '../../assets/ui-design.png'
+import AppDes from '../../assets/app-design.png'
+
 
 const Skill = () => {
   return (
     <section id='skills'>
-      About Section
+      <span className='skillTitle'>What I do</span><br/>
+      <span className='skillDesc'>My major areas of interest include Web Development, Data Structures and Algorithms.</span>
+      <div className='skillBars'>
+        <div className='skillBar'>
+        <img src={UIDesign} alt='skillBarImg' className='skillBarImg'/>
+        <div className='skillBarText'>
+          <h2>
+              Full stack web developer
+          </h2>
+          <p></p>
+        </div>
+        </div>
+
+        <div className='skillBar'>
+        <img src={AppDes} alt='skillBarImg' className='skillBarImg'/>
+        <div className='skillBarText'>
+          <h2>
+              Android App developer
+          </h2>
+          <p></p>
+        </div>
+        </div>
+      </div>
     </section>
   )
 }
